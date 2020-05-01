6db69d0ebe21e0169c8cd180f3fcde6bc6a8fb71
Sascha Englert <sascha@englerts.de>
2020-05-01 11:47:43 +0200

    Added buttons

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
 import { Game } from "./Game";
 import { output } from "./utils";
+import { GameError } from "./GameError";
 
-window.onload = () => {
+window.onload = () => init();
+
+const init = () => {
     try {
-        const game: Game = init();
-        game.start();
+        const game = initGame(16, 25)
+        initButtons(game);
+        
     } catch (error) {
         output(error);
     }
 }
 
-const init = (size?: number): Game => {
-    return new Game(size);
+const initGame = (size?: number, seeds?: number): Game => {
+    return  new Game(size, seeds);
+}
+
+const initButtons = (game: Game) => {
+    const next = document.getElementById("next");
+    if (!next) {
+        throw new GameError("error", "Can't select next button.")
+    }
+
+    next.onclick = game.update;
+
+    const reset = document.getElementById("reset");
+    if (!reset) {
+        throw new GameError("error", "Can't select reset button.")
+    }
+
+    reset.onclick = init;
 }
 
